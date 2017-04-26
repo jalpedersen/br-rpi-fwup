6054e29a21751d97b3f6ff8a18e7431e85b508c1
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-04-26 09:03:56 +0200

    allow user to specify firmware file

--- upgrade_device.sh
+++ upgrade_device.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 target=$1
+firmware=${2:-raspberrypi.fw}
 
 if [ -z $target ]; then
     echo "Usage: $0 target_ip"
     exit 1
 fi
-
-cat raspberrypi.fw | ssh root@$target \
+echo "Flashing $target with $firmware"
+cat $firmware | ssh root@$target \
         'fwup -a -U -d /dev/mmcblk0 -t upgrade && reboot'
