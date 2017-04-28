4a273b8d28142089b30f8a848616f139faeb0e9d
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-04-28 15:44:27 +0200

    play around with device tree overlays

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -6,3 +6,17 @@
 
 #Fix permissions
 chmod 0700 $1/etc/monitrc
+
+env
+echo "Compiling overlays"
+overlaydir=$BINARIES_DIR/overlays
+mkdir -p $overlaydir
+dtc=$BUILD_DIR/linux-53460a0a50f3dd5e60266e945d0ac794ce0eca77/scripts/dtc/dtc
+
+for o in $BR2_EXTERNAL_FWUP_PATH/board/raspberrypi/overlays/*-overlay.dts; do
+  if [ -f $o ]; then
+    file=`basename $o`
+    output=${file%"-overlay.dts"}.dtbo
+    $dtc $o -I dts -@ -O dtb -o $overlaydir/$output
+  fi
+done
