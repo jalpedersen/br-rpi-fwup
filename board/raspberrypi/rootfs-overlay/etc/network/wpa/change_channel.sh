4b5103b170e77c63c0a1fd7c2f2717d0ae0f6798
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-05-11 22:49:34 +0200

    change-hostapd-channel

--- board/raspberrypi/rootfs-overlay/etc/network/wpa/change_channel.sh
+++ board/raspberrypi/rootfs-overlay/etc/network/wpa/change_channel.sh
@@ -2,16 +2,6 @@
 
 iface=$1
 action=$2
-hostapd_init=/etc/init.d/S95hostapd
-conf=/data/hostapd.conf
-
-freq_to_channel() {
-    #https://en.wikipedia.org/wiki/List_of_WLAN_channels#2.4.C2.A0GHz_.28802.11b.2Fg.2Fn.29
-    if [ $1 -ge 2412 ] && [ $1 -le 2472 ]; then
-        chan=`expr \(  $1 - 2412 \) / 5 + 1`
-        echo $chan
-    fi 
-}
 
 if [ "x$action" == "xCONNECTED" ]; then
     sta_freq=`wpa_cli -i wlan0 status | grep freq | cut -c6-`
@@ -20,9 +10,7 @@ if [ "x$action" == "xCONNECTED" ]; then
         exit 1
     fi
     if [ "x$sta_freq" != "x$ap_freq" ]; then
-        chan=`freq_to_channel $sta_freq`
-        sed -i s/channel=.*/channel=$chan/g $conf
-        $hostapd_init restart
+        change-hostapd-channel $sta_freq
     fi
 fi
 
