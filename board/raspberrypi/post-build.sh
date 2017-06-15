71779633347ecf5561a2250d2729522f4f9db5d5
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-06-15 15:28:31 +0200

    update buildroot + linux

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -11,7 +11,7 @@ env
 echo "Compiling overlays"
 overlaydir=$BINARIES_DIR/overlays
 mkdir -p $overlaydir
-dtc=$BUILD_DIR/linux-8b607d1fd1baa4448df39b50cd907fc080e7f960/scripts/dtc/dtc
+dtc=$BUILD_DIR/linux-0926c07dec16ca75cc01c06201c19be7086d3a4b/scripts/dtc/dtc
 
 for o in $BR2_EXTERNAL_FWUP_PATH/board/raspberrypi/overlays/*-overlay.dts; do
   if [ -f $o ]; then
