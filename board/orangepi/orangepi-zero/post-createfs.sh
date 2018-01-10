ed4b9d6f19b0a7658e4c6bef5e6a83758fffaeca
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2018-01-10 11:16:35 +0000

    build uboot.scr

--- board/orangepi/orangepi-zero/post-createfs.sh
+++ board/orangepi/orangepi-zero/post-createfs.sh
@@ -7,6 +7,7 @@ FWUPCONF_NAME=$2
 
 FWUP_CONFIG=$BR2_EXTERNAL_FWUP_PATH/board/orangepi/$FWUPCONF_NAME
 FWUP=$HOST_DIR/usr/bin/fwup
+UBOOT_CONFIG=$BR2_EXTERNAL_FWUP_PATH/board/orangepi/orangepi-zero/boot.cmd
 
 FW_PATH=$BINARIES_DIR/orangepi.fw
 IMG_PATH=$BINARIES_DIR/orangepi.img
@@ -16,6 +17,10 @@ if [ -e $HOST_DIR/usr/bin/mkknlimg ]; then
     $HOST_DIR/usr/bin/mkknlimg \
         $BINARIES_DIR/zImage $BINARIES_DIR/zImage.mkknlimg
 fi
+
+# Create/copy u-boot files to the images directory
+$HOST_DIR/usr/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
+    -n "u-boot script" -d $UBOOT_CONFIG $BINARIES_DIR/boot.scr
 
 # Build the firmware image (.fw file)
 echo "Creating firmware file..."
