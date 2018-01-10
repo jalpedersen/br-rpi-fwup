18746180a0aaeb5a4c0af41b72c5a3be0400e2ba
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2018-01-10 09:36:40 +0000

    fwup for orangepi zero

--- board/orangepi/orangepi-zero/post-build.sh
+++ board/orangepi/orangepi-zero/post-build.sh
@@ -1,1 +1,8 @@
-../post-build.sh
+#!/bin/sh
+# args from BR2_ROOTFS_POST_SCRIPT_ARGS
+# $2    path of boot.cmd
+# $3    output directory for boot.scr
+
+
+#Fix permissions
+chmod 0700 $1/etc/monitrc
