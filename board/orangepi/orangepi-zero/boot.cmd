3c33be270be9cc095c1666f9be25898c955b660d
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2018-01-10 11:23:15 +0000

    orangepi: fix bootargs

--- board/orangepi/orangepi-zero/boot.cmd
+++ board/orangepi/orangepi-zero/boot.cmd
@@ -1,6 +1,6 @@
 setenv fdt_high ffffffff
 
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p1 rootwait
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootfstype=squashfs ro rootwait
 
 fatload mmc 0 $kernel_addr_r zImage
 fatload mmc 0 $fdt_addr_r sun8i-h2-plus-orangepi-zero.dtb
