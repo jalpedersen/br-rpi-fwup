e7a4192b853dfdb895e2a5fe923fe10e4393a736
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2019-01-10 10:04:31 +0100

    bump kernel and firmware

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -11,7 +11,7 @@ env
 echo "Compiling overlays"
 overlaydir=$BINARIES_DIR/overlays
 mkdir -p $overlaydir
-dtc=$BUILD_DIR/linux-4f30a5365296658d66ef8a9f878a25d229e2d5fa/scripts/dtc/dtc
+dtc=$BUILD_DIR/linux-4c5aec7827ac970729ac54e57cbd85fba878a462/scripts/dtc/dtc
 
 for o in $BR2_EXTERNAL_FWUP_PATH/board/raspberrypi/overlays/*-overlay.dts; do
   if [ -f $o ]; then
